244e9dbc7dc143132f43988b7fb58de19e9268c2
PabloGatica <pablo.gaticap@correoaiep.cl>
2023-12-07 19:38:34 -0300

    Primera Version
    Primera Version

---
+++ login.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="es">
+    
+    <head>
+        
+        <meta charset="utf-8">
+        
+        <link rel="stylesheet" href="css/style.css">
+        
+        <style type="text/css">
+            
+        </style>
+
+       
+        
+    </head>
+    
+    <body>
+        <div id="photo">
+        <div id="contenedor">
+            <div id="central">
+                <div id="login">
+                    <div class="titulo">
+                        Bienvenido
+                    </div>
+                    <form id="loginform">
+                        <input type="text" name="usuario" placeholder="Usuario" required>
+                        <i name="bx bx-user"></i>
+                        
+                        <input type="password" placeholder="Contraseña" name="password" required>
+                        <i placeholder="bx bx-lock-alt"></i>
+                        
+                        <button type="submit" title="Ingresar" name="Ingresar">Login</button>
+                    </form>
+                    <div class="pie-form">
+                        <a href="#">¿Perdiste tu contraseña?</a>
+                        <a href="#">¿No tienes Cuenta? Registrate</a>
+                    </div>
+                </div>
+                <div class="inferior">
+                    <a href="#">Volver</a>
+                </div>
+            </div>
+        </div>
+            
+    </body>
+</html>
